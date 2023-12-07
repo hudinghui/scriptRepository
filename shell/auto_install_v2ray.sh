1d8aa8367ac46fa254db45cf63e527716075243f
hudinghui <hu_dinghui@163.com>
2023-12-07 22:10:39 +0800

    fix

--- shell/auto_install_v2ray.sh
+++ shell/auto_install_v2ray.sh
@@ -20,19 +20,26 @@ sudo yum update -y
 sudo yum install curl -y
 
 # 使用官方安装脚本自动安装v2ray
-bash <(curl -L -s https://raw.githubusercontent.com/v2fly/fhs-install-v2ray/master/install-release.sh)
+#bash <(curl -L -s https://raw.githubusercontent.com/v2fly/fhs-install-v2ray/master/install-release.sh)
+#先下载文件，在执行
+wget https://raw.githubusercontent.com/v2fly/fhs-install-v2ray/master/install-release.sh
+bash install-release.sh
 
 # 配置v2ray
 
 # v2ray配置文件路径
-V2RAY_CONFIG="/etc/v2ray/${PORT}/config.json"
+V2RAY_DIR="/etc/v2ray/${PORT}"
+V2RAY_CONFIG="${V2RAY_DIR}/config.json"
 
+#创建文件夹
+mkdir -p /var/log/v2ray
+mkdir -p ${V2RAY_DIR}
 # 写入v2ray配置
 cat <<EOF > ${V2RAY_CONFIG}
 {
   "log" : {
-    "access": "/var/log/v2ray/${PORT}/access.log",
-    "error": "/var/log/v2ray/${PORT}/error.log",
+    "access": "/var/log/v2ray/${PORT}access.log",
+    "error": "/var/log/v2ray/${PORT}error.log",
     "loglevel": "warning"
   },
   "inbounds": [{
@@ -62,6 +69,13 @@ cat <<EOF > ${V2RAY_CONFIG}
 }
 EOF
 
+#指定配置文件
+# V2Ray systemd服务文件路径
+V2RAY_SERVICE_FILE="/etc/systemd/system/v2ray.service"
+# 使用sed命令修改ExecStart行
+sudo sed -i "s|^ExecStart=.*|ExecStart=/usr/bin/v2ray -config ${V2RAY_CONFIG}|" $V2RAY_SERVICE_FILE
+
+
 # 启动v2ray服务
 sudo systemctl enable v2ray
 sudo systemctl start v2ray
@@ -71,7 +85,7 @@ sudo systemctl status v2ray -l --no-pager
 
 # 输出配置信息
 echo "v2ray已安装并配置完成"
-echo "端口: ${PORT}0"
+echo "端口: ${PORT}"
 echo "UUID: ${UUID}"
 echo "alterId: 64"
 echo "传输协议: websocket"
